108ea29a2f1e352c2afe87832838bf3a57495652
Erzis900 <erzis900@gmail.com>
2025-04-29 22:09:02 +0200

    name collision fix

--- RvA/src/main.cpp
+++ RvA/src/main.cpp
@@ -1,12 +1,16 @@
 #define RAYGUI_IMPLEMENTATION
 #include <raygui.h>
 
+#include <raylib_win32.h>
 #include <spdlog/spdlog.h>
 
 #include "Game.h"
 
 int main()
 {
+	SetTraceLogLevel(LOG_ERROR);
+	spdlog::info("Hello from spdlog");
+
 	Game game;
 	game.run();
 
